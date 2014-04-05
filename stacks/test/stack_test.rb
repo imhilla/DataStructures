11654055ad36e4febabe52d75327740acc61d31e
AndreaHK5 <cremese@gmail.com>
2014-04-05 18:41:31 -0400

    turned out I had a bit more time, so why not putting in some more stuff?

--- stacks/test/stack_test.rb
+++ stacks/test/stack_test.rb
@@ -3,27 +3,55 @@ require_relative '../lib/stack.rb'
 
 describe LinkedStack do
 	before :each do
-		@stack = LinkedStack.new
+		@empty_stack = LinkedStack.new
 		@value1 = 1
+		@value2 = 2
 	end
 
-
-	it 'pushes an element at the top increasing the size' do
-		@stack.push(@value1)
-		@stack.top.must_equal @value1
+	it 'responds correctly to empty?' do
+		@empty_stack.empty?.must_equal true
+		@empty_stack.push(@value1)
+		@empty_stack.empty?.must_equal false
 	end
+
+	it 'pushes an element at the top of an empty stack' do
+		@empty_stack.push(@value1)
+		@empty_stack.top.must_equal @value1
+	end
+
+	it 'can return the size' do
+	end
+
+	it 'pushes an element at the top of a stack' do
+		@empty_stack.push(@value1)
+		@empty_stack.push(@value2)
+		@empty_stack.top.must_equal @value2
+	end	
 
 	it ' returns the top element without deleting it from the stack' do
+		@empty_stack.size.must_equal 0
+		@empty_stack.push(@value1)
+		@empty_stack.size.must_equal 1
+	end
+
+	it 'does not pop an empty stack' do
+		@empty_stack.pop.must_equal false
 	end
 
 	it 'pops an element form the top, decreasing the size' do
+		@empty_stack.push(@value1)
+		@empty_stack.pop.must_equal @value1
+		@empty_stack.size.must_equal 0
+		@empty_stack.empty?.must_equal true
 	end
+
+
+
 
 	it 'can tell when it is empty?' do
 	end
 
 
-	it 'can return the size' do
-	end
+
 	
 end
